9ff43ea93aadcfcf43e3a09afe032a63a7c71194
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-17 11:24:03 -0500

    Created index.js with some basic IPC foundations

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+Created by Andrew DeChristopher <drew@kiir.us> on 2/17/2017.
+*/
+
+const ipc = require('node-ipc');
+
+ipc.config.id = 'world';
+ipc.config.retry = 1500;
+
+ipc.serve(function () {
+	ipc.server.on('message', function (data, socket) {
+		ipc.log('got a message : ', data);
+		ipc.server.emit(
+			socket,
+			'message',  // this can be anything you want so long as your client knows.
+			data + ' world!'
+		);
+	});
+	ipc.server.on('socket.disconnected', function (socket, destroyedSocketID) {
+		ipc.log('client ' + destroyedSocketID + ' has disconnected!');
+	});
+});
+
+ipc.server.start();
