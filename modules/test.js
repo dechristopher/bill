9b84bbf5b19f8685a0da3127a084eb908201b508
Andrew DeChristopher <dechristophera@wit.edu>
2017-02-23 17:37:18 -0500

    Improved test class

--- modules/test.js
+++ modules/test.js
@@ -3,22 +3,46 @@ Created by Andrew DeChristopher <drew@kiir.us> on 2/17/2017.
 */
 
 const ipc = require('node-ipc');
+const cron = require('cron');
 
 ipc.config.id = 'hello';
 ipc.config.retry = 1500;
+
+let storedData;
+
+if (process.argv.length > 2) {
+	if (process.argv[2] === 'data') {
+		storedData = 'cooldata';
+		hasData = true;
+	}
+} else {
+	hasData = false;
+}
+
+let showData = cron.job('*/5 * * * * *', function () {
+	console.log('DATA: [' + storedData + ']');
+});
 
 ipc.connectTo('world', function () {
 	ipc.of.world.on('connect', function () {
 		ipc.log('## connected to world ##', ipc.config.delay);
 		ipc.of.world.emit(
-            'message',  // any event or message type your server listens for
-            'hello'
+            'checkData',
+            '1'
         );
+
+		if(hasData) {
+			ipc.of.world.emit('setData', storedData);
+		}
 	});
+
+	ipc.of.world.on('data', function (data) {
+		storedData = data;
+	});
+
 	ipc.of.world.on('disconnect', function () {
 		ipc.log('disconnected from world');
 	});
-	ipc.of.world.on('message', function (data) {
-		ipc.log('got a message from world : ', data);
-	});
 });
+
+showData.start();
